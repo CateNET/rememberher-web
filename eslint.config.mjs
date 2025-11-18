647063343ef172ab681e3aafb4fa5310560ed47b
CateNET Solutions <catenetsolutions@gmail.com>
2025-11-18 02:38:03 -0800

    fixed eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,9 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import eslint from "@eslint/js";
+import next from "eslint-config-next";
+import tseslint from "typescript-eslint";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  next(),
+);
